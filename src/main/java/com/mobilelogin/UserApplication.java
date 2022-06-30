1949b124479a164144ae4260eb599fa4372df132
Akshat1225 <106968317+Akshat1225@users.noreply.github.com>
2022-06-30 12:07:49 +0530

    Update UserApplication.java

--- src/main/java/com/mobilelogin/UserApplication.java
+++ src/main/java/com/mobilelogin/UserApplication.java
@@ -8,22 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-//		ConfigurableApplicationContext context=
 		SpringApplication.run(UserApplication.class, args);
-//      User_Repository userrepo=context.getBean(User_Repository.class);
-//      Book_Repository bookrepo=context.getBean(Book_Repository.class);
-//      com.mobilelogin.data.User user=new com.mobilelogin.data.User();
-//      user.setUserId(3);
-//      user.setEmail("aditya12@gmail.com");
-//      user.setGender("M");
-//      user.setName("aditya");
-//      user.setUserType("normal");
-//      user.setPassword(1234321);
-//      
-//      com.mobilelogin.data.User user1 =userrepo.save(user);
-//      System.out.println(user1);
-//		
-//		System.out.println(User);
 	}
 
 	@Override
