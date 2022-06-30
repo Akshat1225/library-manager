de53d7574a0670af816bfc4f72299a8850783837
Akshat1225 <106968317+Akshat1225@users.noreply.github.com>
2022-06-30 12:04:27 +0530

    Update Book_Repository.java

--- src/main/java/com/mobilelogin/dao/Book_Repository.java
+++ src/main/java/com/mobilelogin/dao/Book_Repository.java
@@ -15,8 +15,6 @@ import com.mobilelogin.data.Book;
 @Repository
 public interface Book_Repository extends JpaRepository<Book,Integer>
 {
-//	@Query(value="SELECT c FROM libary_manger.book c WHERE"+"MATCH(title,category,subject,author)"+"AGAINST(?1)",nativeQuery=true)
-//		public List<Book> getbooks(String title);
 	@Query(value="SELECT b FROM Book b WHERE b.title LIKE %:t% or b.author LIKE %:a% or b.category=:c or b.subject=:s ")
 	public List<Book> getbooksLike(@Param("t")String title,@Param("c")String category,@Param("s")String subject,@Param("a")String author);
 }
